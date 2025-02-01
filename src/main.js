a9998f353140e1847f772e039500d1d941e2a513
Ovidiu <ovidiunoc@gmail.com>
2025-02-01 17:13:11 +0200

    Added Tray, multiple AI models, optimizations

--- src/main.js
+++ src/main.js
@@ -1,12 +1,72 @@
-const { app, BrowserWindow, screen, Menu, globalShortcut, BrowserView } = require('electron')
+const { app, Tray, BrowserWindow, nativeImage, screen, Menu, globalShortcut, BrowserView } = require('electron')
 const path = require('path');
 
 let mainWindow
 let loadingWindow
 let view
+let isWindowVisible = false 
 
 app.setName('DeepSeek4Mac');
 
+function createTray() {
+    const icon = nativeImage.createFromPath(path.join(__dirname, 'resources/icon.png'))
+    tray = new Tray(icon)
+
+    const contextMenu = Menu.buildFromTemplate([
+        {
+            label: 'Toggle Window',
+            click: () => {
+                if (mainWindow.isVisible()) {
+                    mainWindow.hide()
+                } else {
+                    mainWindow.show()
+                }
+            }
+        },
+        {
+            label: 'Select Model',
+            submenu: [
+                {
+                    label: 'DeepSeek',
+                    click: () => {
+                        view.webContents.loadURL("https://chat.deepseek.com");
+                    }
+                },
+                {
+                    label: 'ChatGPT',
+                    click: () => {
+                        view.webContents.loadURL("https://chat.openai.com");
+                        view.webContents.executeJavaScript(`
+                            document.addEventListener('keydown', (e) => {
+                                if (e.key === 'Enter') {
+                                    e.preventDefault();
+                                    const svgElement = document.querySelector('svg.icon-2xl');
+                                    
+                                    if (svgElement) {
+                                        const clickEvent = new MouseEvent('click', {
+                                            bubbles: true,
+                                            cancelable: true,
+                                            view: window
+                                        });
+                                        svgElement.dispatchEvent(clickEvent);
+                                    }
+                                }
+                            });
+                        `);
+                    }
+                }
+            ]
+        },
+        {
+            label: 'Quit', 
+            click: () => {
+                app.quit()
+            }
+        },
+    ])
+
+    tray.setContextMenu(contextMenu)
+}
 
 // display loading window before site is loaded
 function createLoadingWindow() {
@@ -16,6 +76,7 @@ function createLoadingWindow() {
         frame: false,
         transparent: true,
         resizable: false,
+        alwaysOnTop: true,
         webPreferences: {
             nodeIntegration: true,
         }
@@ -52,6 +113,8 @@ function createMainWindow() {
         }
     })
 
+    app.dock.hide()
+
     mainWindow.setTitle('DeepSeek4Mac');
     view = new BrowserView({
         webPreferences: {
@@ -60,7 +123,6 @@ function createMainWindow() {
     });
 
     mainWindow.loadFile(path.join(__dirname, 'pages/overlay.html'))
-
     mainWindow.webContents.on('did-finish-load', () => {
         if (loadingWindow) {
             loadingWindow.close()
@@ -74,8 +136,6 @@ function createMainWindow() {
         view.setBounds({ x: 0, y: 0, width, height });
         view.webContents.loadURL("https://chat.deepseek.com");
         mainWindow.show()
-
-        createMenu();
     })
 
     mainWindow.on('closed', () => { mainWindow = null })
@@ -89,74 +149,8 @@ function createMainWindow() {
     })
 }
 
-function createMenu() {
-    const template = [
-        {
-            label: 'DeepSeek4Mac',
-            submenu: [
-                {
-                    label: 'Refresh',
-                    click: () => {
-                        view.webContents.reload();
-                    }
-                },
-                {
-                    label: 'Force Reload',
-                    click: () => {
-                        view.webContents.reloadIgnoringCache();
-                    }
-                }
-            ]
-        },
-        {
-            label: 'Select Model', 
-            submenu: [
-                {
-                    label: 'DeepSeek',
-                    click: () => {
-                        view.webContents.loadURL("https://chat.deepseek.com");
-                    }
-                },
-                {
-                    label: 'ChatGPT',
-                    click: () => {
-                        view.webContents.loadURL("https://chat.openai.com");
-                        view.webContents.executeJavaScript(`
-                            document.addEventListener('keydown', (e) => {
-                                if (e.key === 'Enter') {
-                                    e.preventDefault();
-                                    const svgElement = document.querySelector('svg.icon-2xl');
-                                    
-                                    if (svgElement) {
-                                        const clickEvent = new MouseEvent('click', {
-                                            bubbles: true,
-                                            cancelable: true,
-                                            view: window
-                                        });
-                                        svgElement.dispatchEvent(clickEvent);
-                                    }
-                                }
-                            });
-                        `);
-                    }
-                }
-            ]
-        }, 
-        {
-            label: 'Edit Options',
-            submenu: [
-                { role: 'Copy' },
-                { role: 'Paste' },
-                { role: 'Cut' },
-                { role: 'Select All' }
-            ]
-        }
-    ];
-
-    const menu = Menu.buildFromTemplate(template);
-    Menu.setApplicationMenu(menu);
-}
 app.whenReady().then(() => {
+    createTray()
     createLoadingWindow()
     createMainWindow()
 
